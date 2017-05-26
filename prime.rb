316be41b133b439985c06164320302093b111637
Thomas Cheung <cheungth25@gmail.com>
2017-05-26 05:13:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  count = 2
+  #check if number is 0, 1 or negative
+  if (number < count)
+    return false
+  end
+  #loop to check if number is prime
+  while count < number do
+    if (number % count == 0)
+      return false
+    end
+    count += 1
+  end
+  return true
+end
